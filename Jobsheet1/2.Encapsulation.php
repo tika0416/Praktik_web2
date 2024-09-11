c2fed6f2923184d9a766a060381259764e48d929
Tika Kuswardani <165125806+tika0416@users.noreply.github.com>
2024-09-12 05:25:25 +0700

    2.Encapsulation.php

--- Jobsheet1/2.Encapsulation.php
+++ Jobsheet1/2.Encapsulation.php
@@ -1,46 +1,46 @@
 <?php 
-//Definisi Class
+// Definisi Class
 class Mahasiswa{
-    //Atribut atau Properties 
+    // Atribut atau Properties 
     private $nama;
     private $nim;
     private $jurusan;
-    //Contructor
-    public function __construct($nama, $nim, $jurusan) {
-        $this->nama = $nama;
-        $this->nim = $nim;
-        $this->jurusan = $jurusan;
-    }
-    //metode atau function getter
+
+    // metode atau function getter
     public function getNama() {
         return $this->nama;
     }
-    //metode atau function setter
+    // metode atau function setter
     public function setNama($nama) {
         $this->nama = $nama;
     }
-    //metode atau function getter
+    // metode atau function getter
     public function getNim() {
         return $this->nim;
     }
-    //metode atau function setter
+    // metode atau function setter
     public function setNim($nim) {
         $this->nim = $nim;
     }
-    //metode atau function getter
+    // metode atau function getter
     public function getJurusan() {
         return $this->jurusan;
     }
-    //metode atau function setter
+    // metode atau function setter
     public function setJurusan($jurusan) {
         $this->jurusan = $jurusan;
     }
 }
-//Instansiasi Objek
-$Mhs = new Mahasiswa("Nama: Tika Kuswardani", "Nim: 230102046", "Jurusan: Komputer dan Bisnis");
-echo $Mhs->getNama(); //Output Pemrograman 
+
+// Instansiasi Objek
+$Mhs = new Mahasiswa();
+$Mhs->setNama("Nama: Tika Kuswardani");
+$Mhs->setNim("Nim: 230102046");
+$Mhs->setJurusan("Jurusan: Komputer dan Bisnis");
+
+echo $Mhs->getNama(); // Output Pemrograman 
 echo "<br>";
-echo $Mhs->getNim(); //Output Pemrograman 
+echo $Mhs->getNim(); // Output Pemrograman 
 echo "<br>";
-echo $Mhs->getJurusan(); //Output Pemrograman 
+echo $Mhs->getJurusan(); // Output Pemrograman 
 ?>
